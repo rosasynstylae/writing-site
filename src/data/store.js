10c48ca2ed50cf601047d2e91c07cef99b411351
Sam <rosasynstylae@gmail.com>
2018-01-30 01:33:17 +0000

    Get redux working

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer';
 
 const logger = createLogger();
@@ -8,7 +9,9 @@ const logger = createLogger();
 const store = createStore(
     rootReducer,
     {},
-    applyMiddleware(thunk, logger)
+    composeWithDevTools(
+        applyMiddleware(thunk, logger)
+    ),
 );
 
 export default store;
